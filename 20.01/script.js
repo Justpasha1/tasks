73b829ac439f3d1bc26b408bff00f93f3c27dfd5
pasha <justuser166@gmail.com>
2023-01-20 09:18:46 +0200

    '1'

--- 20.01/script.js
+++ 20.01/script.js
@@ -1,13 +1,22 @@
 function updateTime() {
+    const  hours = document.querySelector('.hours')
+    const  minutes = document.querySelector('.minutes');
+    const  seconds = document.querySelector('.seconds');
     // Здесь должен быть элемент hours 
     // Здесь должен быть элемент minutes 
     // Здесь должен быть элемент seconds
-
+    console.log(hours,minutes,seconds);
     const clock = new Date();
 
     // getHours()
+    hours.innerHTML = clock.getHours();
     // getMinutes()
+    minutes.innerHTML = clock.getMinutes();
     // getSeconds()
+    seconds.innerHTML = clock.getSeconds();
 
+    
 }
 // Здесь должен быть задан интервал
+setInterval(updateTime, 1000);
+console.log(hours,minutes,seconds);
